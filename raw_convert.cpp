62c580c433b2a5240704ab648700028ccca89bfd
Hamza Khan <75195945+hamzaskhan@users.noreply.github.com>
2024-04-28 12:08:10 +0500

    Update raw_convert.cpp

--- raw_convert.cpp
+++ raw_convert.cpp
@@ -5,48 +5,65 @@
 #include <stdexcept>
 #include <algorithm>
 
+// Forward declaration for classes
+class Vector3D;
+class Matrix3x3;
+
+// Vector3D Class: Represents a 3D vector
 class Vector3D {
 public:
     double x, y, z;
 
+    // Default constructor
     Vector3D() : x(0.0), y(0.0), z(0.0) {}
 
+    // Constructor with initial values
     Vector3D(double xVal, double yVal, double zVal) : x(xVal), y(yVal), z(zVal) {}
 
+    // Vector addition operator
     Vector3D operator+(const Vector3D& other) const {
         return Vector3D(x + other.x, y + other.y, z + other.z);
     }
 
+    // Vector subtraction operator
     Vector3D operator-(const Vector3D& other) const {
         return Vector3D(x - other.x, y - other.y, z - other.z);
     }
 
+    // Dot product of two vectors
     double dotProduct(const Vector3D& other) const {
         return x * other.x + y * other.y + z * other.z;
     }
 
+    // Cross product of two vectors
     Vector3D crossProduct(const Vector3D& other) const {
         return Vector3D(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
     }
 
+    // Get unit vector (vector with magnitude 1)
     Vector3D unitVector() const {
         double magnitude = std::sqrt(x * x + y * y + z * z);
         return Vector3D(x / magnitude, y / magnitude, z / magnitude);
     }
 
+    // Multiply vector by a scalar value
     Vector3D scalarMultiply(double scalar) const {
         return Vector3D(x * scalar, y * scalar, z * scalar);
     }
 };
 
+// Matrix3x3 Class: Represents a 3x3 matrix
 class Matrix3x3 {
 public:
     std::vector<std::vector<double>> data;
 
+    // Default constructor initializes matrix with zeros
     Matrix3x3() : data(3, std::vector<double>(3, 0.0)) {}
 
+    // Constructor with initial data
     Matrix3x3(const std::vector<std::vector<double>>& inputData) : data(inputData) {}
 
+    // Matrix-vector multiplication operator
     Vector3D operator*(const Vector3D& vec) const {
         double resultX = data[0][0] * vec.x + data[0][1] * vec.y + data[0][2] * vec.z;
         double resultY = data[1][0] * vec.x + data[1][1] * vec.y + data[1][2] * vec.z;
@@ -55,6 +72,7 @@ public:
     }
 };
 
+// GuidanceSystem Class: Simulates a guidance system for a space mission
 class GuidanceSystem {
 private:
     int wchPhase;
@@ -69,11 +87,6 @@ private:
 
     // Constants
     const double PI = 3.14159265358979323846;
-    const double GAIN_BRAKING = 1.0;
-    const double TRIM_ACCELERATION = 1.0;
-    const double ZOOM_TIME = 1.0;
-    const double PROJ_MAX = 1.0;
-    const double PROJ_MIN = 1.0;
     const double GRAVITY = 9.81;  // Acceleration due to gravity in m/s^2
     const double SPECIFIC_IMPULSE = 2500.0;  // Specific impulse in seconds
 
@@ -108,14 +121,17 @@ public:
     }
 
     Vector3D operator*(const Vector3D& vec) const {
-        double resultX = data[0][0] * vec.x + data[0][1] * vec.y + data[0][2] * vec.z;
-        double resultY = data[1][0] * vec.x + data[1][1] * vec.y + data[1][2] * vec.z;
-        double resultZ = data[2][0] * vec.x + data[2][1] * vec.y + data[2][2] * vec.z;
+        double resultX = transformationMatrix.data[0][0] * vec.x + transformationMatrix.data[0][1] * vec.y + transformationMatrix.data[0][2] * vec.z;
+        double resultY = transformationMatrix.data[1][0] * vec.x + transformationMatrix.data[1][1] * vec.y + transformationMatrix.data[1][2] * vec.z;
+        double resultZ = transformationMatrix.data[2][0] * vec.x + transformationMatrix.data[2][1] * vec.y + transformationMatrix.data[2][2] * vec.z;
         return Vector3D(resultX, resultY, resultZ);
     }
 
     void calculateAngTerm() {
-        angTerm = vgu + (r * transformationMatrix).scalarMultiply(PI);
+        // Calculate angTerm using vector operations
+        Vector3D vguUnit = vgu.unitVector();  // Get the unit vector of vgu
+        Vector3D crossProd = r.crossProduct(transformationMatrix * vguUnit);  // Compute cross product
+        angTerm = crossProd.scalarMultiply(PI);  // Multiply by PI to get angTerm
     }
 
     void computeCg() {
@@ -165,19 +181,15 @@ public:
 
     void integrateLunarLandingEquations() {
         // Lunar landing constants
-        const double TSCALINV = 1.0 / TTFSCALE;
+        const double TSCALINV = 1.0 / TSCALINV;  // Replace TTFSCALE with TSCALINV
         const double DEC103 = -103.0;
-        const double DEC99 = 99.0;
         const double TREDESCL = -0.08;
-        const double NEGMAX = -1.0; // Define NEGMAX value
-        const double POSMAX = 1.0; // Define POSMAX value
 
         // Example calculation using provided assembly code comments
-        double result = ttfOver8 / 8.0 * TREDESCL - DEC103 + NEGMAX + POSMAX;
+        double result = ttfOver8 / 8.0 * TREDESCL - DEC103;
         std::cout << "Result: " << result << std::endl;
     }
 
-    // New functions
     double calculateVelocity(double initialVelocity, double acceleration, double time) {
         return initialVelocity + acceleration * time;
     }
@@ -238,9 +250,6 @@ int main() {
     double time = 5.0;  // seconds
     double velocity = guidanceSystem.calculateVelocity(initialVelocity, acceleration, time);
     std::cout << "Velocity after " << time << " seconds: " << velocity << " m/s" << std::endl;
-
-    // Other calculations and functions can be similarly used
-    // ...
 
     return 0;
 }
